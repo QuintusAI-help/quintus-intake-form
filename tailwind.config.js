69294695db56287c55f47846e1d9d1a3cad98e0d
QuintusAI-help <quintushelp@gmail.com>
2026-06-16 17:10:15 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0D1B2A',
+        gold: '#C9A84C',
+      },
+      fontFamily: {
+        serif: ['var(--font-cormorant)', 'Georgia', 'serif'],
+        sans: ['var(--font-inter)', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
